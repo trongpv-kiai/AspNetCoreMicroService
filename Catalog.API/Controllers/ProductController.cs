99f79810be99ce65f1fad259741fdc699a816b79
Your Name <trong.pham@kiaisoft.com>
2023-10-02 17:52:29 +0700

    feat: add logic crud of catalog service

---
+++ Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(
+            IProductRepository repository,
+            ILogger<ProductController> logger
+            )
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(await _repository.GetProducts());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(string id)
+        {
+            return Ok(await _repository.GetProduct(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            return Ok(await _repository.CreateProduct(product));
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult<Product>> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(string id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
+        }
+    }
+}
---
+++ Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration _configuration)
+        {
+            var client = new MongoClient(_configuration.GetSection("DatabaseSetting").GetSection("ConnectionString").Value);
+            var database = client.GetDatabase(_configuration.GetSection("DatabaseSetting").GetSection("DatabaseName").Value);
+
+            Products = database.GetCollection<Product>(_configuration.GetSection("DatabaseSetting").GetSection("CollectionName").Value);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
